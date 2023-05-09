a3c4f71cca5220df323bf4b9a08ba503acfc19b3
Matias Arias <matt.arias182@gmail.com>
2023-05-09 17:33:06 -0300

    se agrego componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav className="h-[100px] w-full bg-gradient-to-r from-teal-800 via-green-800 to-emerald-950 shadow-md shadow-amber-950">
+        <div className="h-full w-full flex justify-end items-center gap-12 px-16 py-4">
+          <Link to={"/"}>
+            <span className="text-gray-100 text-xl xl:text-2xl font-bold hover:text-amber-800">
+              Home
+            </span>
+          </Link>
+
+          <Link to={"/about"}>
+            <span className="text-gray-100 text-xl xl:text-2xl font-bold hover:text-amber-800">
+              About
+            </span>
+          </Link>
+
+          <Link to={"contact"}>
+            <span className="text-gray-100 text-xl xl:text-2xl font-bold hover:text-amber-800">
+              Contact
+            </span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
