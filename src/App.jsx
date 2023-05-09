25592baaa56cc7abd9d1ff02caba36ef511ed655
Matias Arias <matt.arias182@gmail.com>
2023-05-09 15:51:56 -0300

    se instalaron todas las dependecias para comenzar el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-green-600 text-2xl font-bold">Redux practice</h1>
+    </div>
+  );
+};
+
+export default App;
