9e3d6c373653874e147ece83586b96ed29acf3e2
Matias Arias <matt.arias182@gmail.com>
2023-05-10 14:30:41 -0300

    se cambio las font sizes de los links del navbar en responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <header>
-      <nav className="h-[100px] w-full bg-gradient-to-r from-teal-800 via-green-800 to-emerald-950 shadow-md shadow-amber-950">
-        <div className="h-full w-full flex justify-end items-center gap-12 px-16 py-4">
+      <nav className="h-auto sm:h-[100px] w-full bg-gradient-to-r from-teal-800 via-green-800 to-emerald-950 shadow-md shadow-amber-950">
+        <div className="h-full w-full flex flex-col sm:flex-row justify-center md:justify-end items-center gap-12 px-16 py-4">
           <Link to={"/"}>
-            <span className="text-gray-100 text-xl xl:text-2xl 2xl:text-3xl font-bold hover:text-amber-800">
+            <span className="text-gray-100 text-lg sm:text-xl lg:text-2xl 2xl:text-3xl font-bold hover:text-amber-800">
               Inicio
             </span>
           </Link>
 
           <Link to={"/about"}>
-            <span className="text-gray-100 text-xl xl:text-2xl 2xl:text-3xl font-bold hover:text-amber-800">
+            <span className="text-gray-100 text-lg sm:text-xl lg:text-2xl 2xl:text-3xl font-bold hover:text-amber-800">
               Sobre mi
             </span>
           </Link>
 
           <Link to={"contact"}>
-            <span className="text-gray-100 text-xl xl:text-2xl 2xl:text-3xl font-bold hover:text-amber-800">
+            <span className="text-gray-100 text-lg sm:text-xl lg:text-2xl 2xl:text-3xl font-bold hover:text-amber-800">
               Contacto
             </span>
           </Link>
